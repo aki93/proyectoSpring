4a7cc7080814e1e4279c4fe6d8f32064586cdbf5
Akira <morinaka.aki@gmail.com>
2020-09-02 22:25:41 -0300

    se creo interfaz de categorias

--- src/main/java/net/akira/controller/CategoriasController.java
+++ src/main/java/net/akira/controller/CategoriasController.java
@@ -1,33 +1,52 @@
 
 package net.akira.controller;
 
+import java.util.List;
+import net.akira.model.Categoria;
+import net.akira.service.ICategoriasService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(value="/categorias")
 public class CategoriasController {
     
-    // @GetMapping("/index")
-@RequestMapping(value="/index", method=RequestMethod.GET)
-public String mostrarIndex(Model model) {
- return "categorias/listCategorias";
-}
-// @GetMapping("/create")
-@RequestMapping(value="/create", method=RequestMethod.GET)
-public String crear() {
-  return "categorias/formCategoria";
-}
-// @PostMapping("/save")
-@RequestMapping(value="/save", method=RequestMethod.POST)
-public String guardar(@RequestParam("nombre")String nombre,@RequestParam("descripcion")String descripcion) {
-  System.out.println("Categoria:" + nombre);
-  System.out.println("Descripcion: " + descripcion);
-    return "categorias/listCategorias";
-}
+    @Autowired
+    private ICategoriasService serviceCategorias;
 
     
+    @GetMapping("/index")
+    public String mostrarIndex(Model model) {
+        List<Categoria> lista = serviceCategorias.buscarTodas();
+        model.addAttribute("categorias", lista);
+        return "categorias/listCategorias";
+    }
+    @GetMapping("/create")
+    public String crear(Categoria categoria) {
+        return "categorias/formCategoria";
+    }
+    @PostMapping("/save")
+    public String guardar(Categoria categoria, BindingResult result, RedirectAttributes attributes) {
+        if(result.hasErrors()){
+        for(ObjectError error: result.getAllErrors()){
+    System.out.println("Error:" + error.getDefaultMessage());
+    }
+        return "categorias/formCategoria";
+        }
+        serviceCategorias.guardar(categoria);
+        attributes.addFlashAttribute("msg","Registro Guardado");
+        System.out.println(categoria);
+      
+        return "redirect:/categorias/index";
+    }
+
 }
---
+++ src/main/java/net/akira/service/CategoriasServiceImpl.java
@@ -0,0 +1,74 @@
+
+package net.akira.service;
+
+import java.util.LinkedList;
+import java.util.List;
+import net.akira.model.Categoria;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriasServiceImpl implements ICategoriasService {
+    
+    private List<Categoria> lista = null;
+    
+    public CategoriasServiceImpl (){
+    lista = new LinkedList<Categoria>();
+    
+    Categoria categoria1 = new Categoria();
+    categoria1.setId(1);
+    categoria1.setNombre("Ventas");
+    categoria1.setDescripcion("Trabajos en sector Ventas");
+    
+    Categoria categoria2 = new Categoria();
+    categoria2.setId(2);
+    categoria2.setNombre("Contabilidad");
+    categoria2.setDescripcion("Trabajos en sector Contabilidad");
+    
+    Categoria categoria3 = new Categoria();
+    categoria3.setId(3);
+    categoria3.setNombre("Comunicacion");
+    categoria3.setDescripcion("Trabajos en sector Comunicacion");
+    
+    Categoria categoria4 = new Categoria();
+    categoria4.setId(4);
+    categoria4.setNombre("Arquitectura");
+    categoria4.setDescripcion("Trabajos en sector Arquitectura");
+    
+    Categoria categoria5 = new Categoria();
+    categoria5.setId(5);
+    categoria5.setNombre("Educacion");
+    categoria5.setDescripcion("Trabajps en Educacion");
+    
+    lista.add(categoria1);
+    lista.add(categoria2);
+    lista.add(categoria3);
+    lista.add(categoria4);
+    lista.add(categoria5);
+  
+    }
+
+    @Override
+    public void guardar(Categoria categoria) {
+        lista.add(categoria);
+        
+    }
+
+    @Override
+    public List<Categoria> buscarTodas() {
+        return lista;
+    }
+
+    @Override
+    public Categoria buscarPorId(Integer idCategoria) {
+        
+        for (Categoria c : lista)
+            if(c.getId() == idCategoria){
+        return c;
+    }
+        
+        return null;
+    }
+    
+    
+    
+}
